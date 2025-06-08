3f7e76f4161bcc261432b9bc0547302641615146
donkererubs <github@rubendonker.com>
2025-06-08 17:41:59 +0200

    Added initial chat functionality

---
+++ GatheringTheMagic.Infrastructure/RealTime/InMemoryChatHistoryService.cs
@@ -0,0 +1,20 @@
+﻿using GatheringTheMagic.Application.Services;
+using System.Collections.Concurrent;
+
+namespace GatheringTheMagic.Infrastructure.RealTime;
+
+public class InMemoryChatHistoryService : IChatHistoryService
+{
+    private readonly List<ChatMessage> _messages = new();
+    private readonly object _lock = new();
+
+    public void AddMessage(ChatMessage message)
+    {
+        lock (_lock) { _messages.Add(message); }
+    }
+
+    public IReadOnlyList<ChatMessage> GetHistory()
+    {
+        lock (_lock) { return _messages.ToList(); }
+    }
+}
